edaaa6b929926a764cc8ba59ff3ce35f377acc12
PetrovDmitriyV <phanl2@mail.ru>
2024-10-21 22:28:42 +0300

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -1,47 +1,42 @@
 class Vehicle():
-    owner = []
-    __model = []
-    __engine_power = []
-    __color = []
+    owner = str()
+    __model = str()
+    __engine_power = int()
+    __color = str()
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def get_model(self, model):
-        self.model = str(model)
-        __model = self.model
+    def __init__(self, owner, model, color, power):
+        self.owner = owner
+        self.__color = color
+        self.__model = model
+        self.__engine_power = power
+
+    def get_model(self, __model):
         print(f'Модель: {__model}')
 
-    def get_horsepower(self, power):
-        self.power = int(power)
-        __engine_power = self.power
+    def get_horsepower(self, __engine_power):
         print(f'Мощность двигателя: {__engine_power}')
 
-    def get_color(self, color):
-        self.color = str(color)
-        __color = self.color
+    def get_color(self, __color):
         print(f'Цвет: {__color}')
 
     def print_info(self):
-        print(f'Модель: {self.model}')
-        print(f'Мощность двигателя: {self.power}')
-        print(f'Цвет: {self.color}')
+        print(f'Модель: {self.__model}')
+        print(f'Мощность двигателя: {self.__engine_power}')
+        print(f'Цвет: {self.__color}')
         print(f'Владелец: {self.owner}')
 
     def set_color(self, new_color):
         self.new_color = str(new_color)
         new_color_ = new_color.lower()
         if new_color_ in Vehicle.__COLOR_VARIANTS:
-            self.color = new_color
+            self.__color = new_color
         else:
             print(f'Нельзя сменить цвет на {new_color}')
 
 
 class Sedan(Vehicle):
-
-    def __init__(self, owner, model, color, power):
-        self.owner = owner
-        self.model = model
-        self.power = power
-        self.color = color
+    pass
 
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
